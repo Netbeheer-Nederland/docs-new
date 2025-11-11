e3f16c221254c1871c1ad09ed04ab10fb7625972
Bart Kleijngeld <bartkl@gmail.com>
2025-11-11 15:30:56 +0100

    Hide 'terug naar startpagina' in geval je er al bent

--- supplemental-ui/js/add-back-to-home-nav-item.js
+++ supplemental-ui/js/add-back-to-home-nav-item.js
@@ -1,22 +1,28 @@
-const ul = document.querySelector('nav.nav-menu ul');
+(function () {
+  const navContainer = document.querySelector('body > .nav-container')
+  if (navContainer.dataset.component !== 'ROOT')
+    return
 
-// Create a new <li> element.
-const li = document.createElement('li');
-li.classList.add('nav-item');
-li.dataset.depth = "0"
+  const ul = document.querySelector('nav.nav-menu ul');
 
-const backToHome = document.createElement('a');
-backToHome.href = '/';
-backToHome.classList.add('nav-link');
-backToHome.textContent = 'Terug naar startpagina';
+  // Create a new <li> element.
+  const li = document.createElement('li');
+  li.classList.add('nav-item');
+  li.dataset.depth = "0"
 
-// Add `↰`.
-const backToHomeSymbol = document.createElement('span');
-backToHomeSymbol.classList.add('nav-symbol');
-backToHomeSymbol.textContent = '↰ ';
+  const backToHome = document.createElement('a');
+  backToHome.href = '/';
+  backToHome.classList.add('nav-link');
+  backToHome.textContent = 'Terug naar startpagina';
 
-li.appendChild(backToHomeSymbol)
-li.appendChild(backToHome)
+  // Add `↰`.
+  const backToHomeSymbol = document.createElement('span');
+  backToHomeSymbol.classList.add('nav-symbol');
+  backToHomeSymbol.textContent = '↰ ';
 
-// Prepend it to the <ul>.
-ul.prepend(li);
+  li.appendChild(backToHomeSymbol)
+  li.appendChild(backToHome)
+
+  // Prepend it to the <ul>.
+  ul.prepend(li);
+})();
